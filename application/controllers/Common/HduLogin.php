6195d1a7668400438e263a105e79b5b5ca65ff50
jiangbaiyan <987082332@qq.com>
2018-12-11 16:50:26 +0800

    升级框架

---
+++ application/controllers/Common/HduLogin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baiyan
+ * Date: 2018-12-11
+ * Time: 12:50
+ */
+
+class Common_HduLogin extends BaseController{
+
+
+    /**
+     * 参数校验
+     */
+    protected function checkParam()
+    {
+        // TODO: Implement checkParam() method.
+    }
+
+    /**
+     * 业务逻辑
+     */
+    protected function indexAction()
+    {
+        // TODO: Implement indexAction() method.
+    }
+
+}
